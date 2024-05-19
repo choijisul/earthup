5a152555b640b4025f90f657f7f549c48e09fcad
최지설 <121646920+choijisul@users.noreply.github.com>
2024-05-20 08:54:21 +0900

    마이페이지 아이디 뜨게, 로그아웃 버튼 사용자 정보(참여, 좋아요)는 아직 없음

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php require 'db.php'; ?>
-<?php require 'auth.php'?>  <!--dp접근 php, 쿠키 관련 php 가져옴-->
+<?php require 'auth.php' ?> <!--dp접근 php, 쿠키 관련 php 가져옴-->
 
 <!DOCTYPE html>
 <html lang="en">
 <!--테스트-->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/homeNav.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/myPage.css">
     <title>어썹</title>
 </head>
 
@@ -26,22 +28,36 @@
             <!-- $authenticated -->
             <i onClick="location.href='http://localhost/camera.php'" class="garbageSearch">쓰레기 검색</i>
             <i onClick="location.href='http://localhost/ploggingBoard.php'" class="proggingBoard">플로깅 게시판</i>
-            <?php if($authenticated == true): ?>
-                <i onClick="location.href='http://localhost/logout.php'" class="membership"> <?php echo $loginId; ?>로그아웃</i>
-            <?php else: ?>
-            <i onClick="location.href='http://localhost/membership.php'" class="membership">회원가입</i>
-            <i onClick="location.href='http://localhost/login.php'" class="login">로그인</i>
+            <?php if ($authenticated == true) : ?>
+                <button class="myPage">사진</button>
+                <div class="menu">
+                    <div class="section" id="section1">
+                        <span><?php echo $loginId; ?></span>
+                        <button class="logout" onClick="location.href='http://localhost/logout.php'">로그아웃</button>
+                    </div>
+                    <hr class="hr">
+                    <div class="section" id="section2">
+                        <h1 class="title">마이페이지</h1>
+                        <a href="#plogging1">참여한 플로깅</a>
+                        <a href="#plogging2">좋아요한 플로깅</a>
+                        <a href="#posts">좋아요한 글</a>
+                    </div>
+                </div>
+
+            <?php else : ?>
+                <i onClick="location.href='http://localhost/membership.php'" class="membership">회원가입</i>
+                <i onClick="location.href='http://localhost/login.php'" class="login">로그인</i>
             <?php endif; ?>
         </h1>
     </div>
-    
+
     <main class="main">
         <div id="banner-container">
             <img src="img/나무.jpg" class="banner-image" alt="">
             <img src="img/숲속.jpg" class="banner-image" alt="">
             <img src="img/나무2.jpg" class="banner-image" alt="">
         </div>
-    
+
         <div class="bottom-part">
             <div class="array">
                 <div class="leftzone animated">
@@ -51,12 +67,9 @@
                     <div class="textzone">
                         <div class="textzone">
                             <p>쓰레기를 찍어 쉽게<br> 분리수거를 해보세요.<br>
-                            쓰레기를 버리는 방법을<br> 터득하여 지구를 보호해주세요.</p>
+                                쓰레기를 버리는 방법을<br> 터득하여 지구를 보호해주세요.</p>
                         </div>
-
-
-                        
-                    </div>                    
+                    </div>
                 </div>
             </div>
         </div>
@@ -81,7 +94,8 @@
                 </div>
                 <div class="rightzone animated">
                     <div class="textzone">
-                        <p class="earth-responsibility"><p>우리가 만든 쓰레기,<br> 우리가 책임지는 지구.</b></p>
+                        <p class="earth-responsibility">
+                        <p>우리가 만든 쓰레기,<br> 우리가 책임지는 지구.</b></p>
 
                     </div>
                 </div>
@@ -90,6 +104,6 @@
     </main>
 </body>
 <script src="js/index.js"></script>
+<script src="js/script.js"></script>
+
 </html>
-
-
