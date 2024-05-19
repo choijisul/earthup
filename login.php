72c0759e58d01462c5a0710b4ff89fab99f603a1
최지설 <121646920+choijisul@users.noreply.github.com>
2024-05-19 17:11:18 +0900

    로그인, 회원가입 버튼 주소 수정

--- login.php
+++ login.php
@@ -1,41 +1,36 @@
-<?php
-echo "<!DOCTYPE html>\n";
-echo "<html lang=\"en\">\n";
-echo "<head>\n";
-echo "    <meta charset=\"UTF-8\">\n";
-echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-echo "    <link rel=\"stylesheet\" href=\"css/loginMembershipNav.css\">\n";
-echo "    <link rel=\"stylesheet\" href=\"css/login.css\">\n";
-echo "\n";
-echo "\n";
-echo "    <title>로그인</title>\n";
-echo "</head>\n";
-echo "<body>\n";
-echo "\n";
-echo "    <!-- nav -->\n";
-echo "    <header>\n";
-echo "        <div class=\"nav_container\">\n";
-echo "            <h1>\n";
-echo "                <p><button onClick=\"location.href=\'index.html\'\" class=\"backButton\"><img\n";
-echo "                    src=\"./img/backButton.png\"></button>\n";
-echo "            </h1>\n";
-echo "        </div>\n";
-echo "    </header>\n";
-echo "\n";
-echo "    <!-- 로고 이미지 -->\n";
-echo "    <div class=\"logo\">로고</div>\n";
-echo "\n";
-echo "    <!-- 아이디 입력창 -->\n";
-echo "    <input type=\"text\" placeholder=\"아이디\" class=\"input-id\">\n";
-echo "    <!-- 비밀번호 입력창 -->\n";
-echo "    <input type=\"text\" placeholder=\"비밀번호\" class=\"input-pw\">\n";
-echo "\n";
-echo "    <!-- 로그인 버튼 -->\n";
-echo "    <button class=\"loginButton\">로그인</button>\n";
-echo "\n";
-echo "    <i onClick=\"location.href=\'membership.html\'\" class=\"goMembership\">회원가입</i>\n";
-echo "\n";
-echo "</body>\n";
-echo "<script src=\"js/index.js\"></script>\n";
-echo "</html>\n";
-?>
+<?php require 'db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loginMembershipNav.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>로그인</title>
+</head>
+<body>
+    <!-- nav -->
+    <header>
+        <div class="nav_container">
+            <h1>
+                <p><button onClick="location.href='http://localhost/index.php'" class="backButton"><img src="./img/backButton.png"></button></p>
+            </h1>
+        </div>
+    </header>
+
+    <!-- 로고 이미지 -->
+    <div class="logo">로고</div>
+
+    <!-- 아이디 입력창 -->
+    <input type="text" placeholder="아이디" class="input-id">
+    <!-- 비밀번호 입력창 -->
+    <input type="password" placeholder="비밀번호" class="input-pw">
+
+    <!-- 로그인 버튼 -->
+    <button class="loginButton" onClick="location.href='http://localhost/index.php'">로그인</button>
+
+    <i onClick="location.href='http://localhost/membership.php'" class="goMembership">회원가입</i>
+
+    <script src="js/index.js"></script>
+</body>
+</html>
--- membership.php
+++ membership.php
@@ -1,67 +1,58 @@
-<?php
-echo "<!DOCTYPE html>\n";
-echo "<html lang=\"en\">\n";
-echo "<head>\n";
-echo "    <meta charset=\"UTF-8\">\n";
-echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-echo "    <link rel=\"stylesheet\" href=\"css/membership.css\">\n";
-echo "    <link rel=\"stylesheet\" href=\"css/loginMembershipNav.css\">\n";
-echo "    \n";
-echo "    <title> 회원가입</title>\n";
-echo "</head>\n";
-echo "<body>\n";
-echo "    <!-- nav -->\n";
-echo "    <header>\n";
-echo "        <div class=\"nav_container\">\n";
-echo "            <h1>\n";
-echo "                <p><button onClick=\"location.href=\'index.html\'\" class=\"backButton\"><img\n";
-echo "                    src=\"./img/backButton.png\"></button>\n";
-echo "            </h1>\n";
-echo "        </div>\n";
-echo "    </header>\n";
-echo "    <div class=\"header_container\">\n";
-echo "        <h1>\n";
-echo "            <i onClick=\"location.href=\'camera.html\'\" class=\"garbageSearch\">쓰레기 검색</i>   \n";
-echo "            <i onClick=\"location.href=\'froggingBoard.html\'\" class=\"froggingBoard\">플로깅 게시판</i>\n";
-echo "            <i onClick=\"location.href=\'membership.html\'\" class=\"membership\">회원가입</i>\n";
-echo "            <i onClick=\"location.href=\'login.html\'\" class=\"login\">로그인</i>\n";
-echo "        </h1>\n";
-echo "    </div>\n";
-echo "\n";
-echo "    <!-- 로그인 회색 상자 -->\n";
-echo "    <div class=\"login_container\"></div>\n";
-echo "\n";
-echo "    <!-- 입력 -->\n";
-echo "    <div class=\"input\">\n";
-echo "        <!-- 아이디 -->\n";
-echo "        <div class=\"member-id\">\n";
-echo "            <div class=\"id\">아이디</div>\n";
-echo "            <input type=\"text\" placeholder=\"아이디\" class=\"input-id\">\n";
-echo "        </div>\n";
-echo "        \n";
-echo "        <!-- 비밀번호 -->\n";
-echo "        <div class=\"member-pw\">\n";
-echo "            <div class=\"pw\">비밀번호</div>\n";
-echo "            <input type=\"text\" placeholder=\"비밀번호\" class=\"input-pw\">\n";
-echo "        </div>\n";
-echo "\n";
-echo "        <!-- 비밀번호 재확인-->\n";
-echo "        <div class=\"member-reconfirm\">\n";
-echo "            <div class=\"reconfirm\">비밀번호 재확인</div>\n";
-echo "            <input type=\"text\" placeholder=\"비밀번호 재확인\" class=\"input-reconfirm\">\n";
-echo "        </div>\n";
-echo "\n";
-echo "        <!-- 이메일 -->\n";
-echo "        <div class=\"member-email\">\n";
-echo "            <div class=\"email\">이메일</div>\n";
-echo "            <input type=\"text\" placeholder=\"이메일\" class=\"input-email\">\n";
-echo "        </div>\n";
-echo "    </div>\n";
-echo "\n";
-echo "    <button class=\"membershipButton\">회원가입</button>\n";
-echo "    \n";
-echo "    \n";
-echo "</body>\n";
-echo "<script src=\"js/index.js\"></script>\n";
-echo "</html>\n";
-?>
+<?php require 'db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/membership.css">
+    <link rel="stylesheet" href="css/loginMembershipNav.css">
+    <title>회원가입</title>
+</head>
+
+<body>
+    <!-- nav -->
+    <header>
+        <div class="nav_container">
+            <h1>
+                <p><button onClick="location.href='http://localhost/index.php'" class="backButton"><img src="./img/backButton.png"></button></p>
+            </h1>
+        </div>
+    </header>
+
+    <!-- 로그인 회색 상자 -->
+    <div class="login_container"></div>
+
+    <!-- 입력 -->
+    <div class="input">
+        <!-- 아이디 -->
+        <div class="member-id">
+            <div class="id">아이디</div>
+            <input type="text" placeholder="아이디" class="input-id">
+        </div>
+
+        <!-- 비밀번호 -->
+        <div class="member-pw">
+            <div class="pw">비밀번호</div>
+            <input type="password" placeholder="비밀번호" class="input-pw">
+        </div>
+
+        <!-- 비밀번호 재확인-->
+        <div class="member-reconfirm">
+            <div class="reconfirm">비밀번호 재확인</div>
+            <input type="password" placeholder="비밀번호 재확인" class="input-reconfirm">
+        </div>
+
+        <!-- 이메일 -->
+        <div class="member-email">
+            <div class="email">이메일</div>
+            <input type="email" placeholder="이메일" class="input-email">
+        </div>
+    </div>
+
+    <button class="membershipButton" onClick="location.href='http://localhost/login.php'">회원가입</button>
+
+    <script src="js/index.js"></script>
+</body>
+
+</html>
