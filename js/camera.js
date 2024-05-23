c2299a8e6d0dc9b0f8fa8331b7871cf528841328
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-05-23 23:33:25 +0900

    인식버튼, 안내문구

--- js/camera.js
+++ js/camera.js
@@ -1,3 +1,4 @@
+// 안내문구 3초 뒤에 자동삭제
 document.addEventListener("DOMContentLoaded", function () {
     // 3초 후에 .message 요소를 숨김
     setTimeout(function () {
@@ -6,4 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
             messageElement.style.display = 'none';
         }
     }, 3000); // 3초
+});
+
+// 인식 확인
+document.addEventListener("DOMContentLoaded", function() {
+    var recognitionButton = document.querySelector('.btn-camera');
+    var messageElement = document.querySelector('.message');
+
+    recognitionButton.addEventListener('click', function() {
+        alert('인식 중 입니다!');
+        messageElement.style.display = 'block';
+    });
 });
