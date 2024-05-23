19dbdc2801179bf3bea9ba21558b1e50d07050b3
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-05-23 18:37:57 +0900

    에러 수정

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $username = "earthup";
 $password = "earthup2024";
 $dbname = "earthup";
 
-$conn = new mysqli($servername, $username, $password, $dbname);  //DB연결 생성
+$conn = new mysql($servername, $username, $password, $dbname);  //DB연결 생성
 
 if ($conn->connect_error) {
   echo "<script>console.error('연결 실패: " . $conn->connect_error . "');</script>";
