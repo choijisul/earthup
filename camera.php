8ee1c9109c1c2c00e125fe345c0401ba71bdcf53
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2024-05-31 15:25:46 +0900

    commit

--- camera.php
+++ camera.php
@@ -12,62 +12,10 @@
     <!-- 안내문구 -->
     <div class="message">버릴 쓰레기를 인식해주세요!</div>
 
-<<<<<<< HEAD
-        // the link to your model provided by Teachable Machine export panel
-        const URL = "./my_model/";
-
-        let model, webcam, labelContainer, maxPredictions;
-
-        // Load the image model and setup the webcam
-        async function init() {
-            const modelURL = URL + "model.json";
-            const metadataURL = URL + "metadata.json";
-
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-            model = await tmImage.load(modelURL, metadataURL);
-            maxPredictions = model.getTotalClasses();
-
-            // Convenience function to setup a webcam
-            const flip = true; // whether to flip the webcam
-            webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-            await webcam.setup(); // request access to the webcam
-            await webcam.play();
-            window.requestAnimationFrame(loop);
-
-            // append elements to the DOM
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
-            labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
-                labelContainer.appendChild(document.createElement("div"));
-            }
-        }
-
-        async function loop() {
-            webcam.update(); // update the webcam frame
-            await predict();
-            window.requestAnimationFrame(loop);
-        }
-        
-        // run the webcam image through the image model
-        async function predict() {
-            // predict can take in an image, video or canvas html element
-            const prediction = await model.predict(webcam.canvas);
-            for (let i = 0; i < maxPredictions; i++) {
-                const classPrediction =
-                    prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-                labelContainer.childNodes[i].innerHTML = classPrediction;
-            }
-        }
-    </script>
-=======
     <!-- 인식버튼 -->
     <button class="btn-camera">
         <img src="img/btn-camera.png" alt="인식버튼">
     </button>
->>>>>>> 4ab057899b2af53050b9733181bf9af9d0e39a45
 
     <!-- 사진 불러오기 -->
     <button class="btn-image" onclick="document.getElementById('fileInput').click()">
