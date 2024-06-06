f07d89c6f9737ee01cab417b141eb34f0c576772
minse2410 <w2209@e-mirim.hs.kr>
2024-06-07 06:03:12 +0900

    계속되는 실패.. 우울하다

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   AOS.init({
-    duration: 1200,
-  })
+      duration: 1200,
+  });
+
   // 모든 배너 이미지 요소를 선택
   let images = document.querySelectorAll(".banner-image");
   // 현재 이미지의 인덱스를 저장하는 변수
@@ -11,18 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // 주어진 인덱스의 이미지를 보여주는 함수
   function showImage(index) {
-    // 모든 이미지를 숨김
-    images.forEach(image => image.style.opacity = 0);
-    // 주어진 인덱스의 이미지를 표시
-    images[index].style.opacity = 1;
+      // 모든 이미지를 숨김
+      images.forEach(image => image.style.opacity = 0);
+      // 주어진 인덱스의 이미지를 표시
+      images[index].style.opacity = 1;
   }
 
   // 다음 이미지로 이동하는 함수
   function nextImage() {
-    // 다음 이미지의 인덱스를 계산하고 배열 길이로 나누어 순환
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    // 다음 이미지를 표시
-    showImage(currentImageIndex);
+      // 다음 이미지의 인덱스를 계산하고 배열 길이로 나누어 순환
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      // 다음 이미지를 표시
+      showImage(currentImageIndex);
   }
 
   // 일정한 간격으로 다음 이미지로 전환하는 타이머 설정
@@ -31,11 +32,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var logo = document.querySelector(".logName");
 
-    // When the user clicks on the logo, navigate to the footer
-    logo.addEventListener("click", function() {
-        window.location.href = "#footer";
-    });
+// When the user clicks on the logo, navigate to the footer
+logo.addEventListener("click", function() {
+  window.location.href = "#footer";
+});
 
 
-
-    
