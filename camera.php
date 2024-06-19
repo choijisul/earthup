c3ec2f2ccbf7bb9968efa13036d721c81d43dc3d
최지설 <121646920+choijisul@users.noreply.github.com>
2024-06-19 10:58:07 +0900

    카메라 인식 전 안내문구 5초, 카메라 인식 3초, css 수정

--- camera.php
+++ camera.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>어썹</title>
-    <link rel="stylesheet" href="css/camera.css">
+    <link rel="stylesheet" href="css/camera.css?val1">
     <link rel="icon" href="img/pavicon.png" type="image/png" sizes="32x32">
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@latest/dist/teachablemachine-image.min.js"></script>
 </head>
 
 <script>
-    document.addEventListener("DOMContentLoaded", async function () {
+    document.addEventListener("DOMContentLoaded", async function() {
         // 첫 로드 시 안내문구 자동 삭제
-        setTimeout(function () {
+        setTimeout(function() {
             var messageElement = document.querySelector('.message');
             if (messageElement) {
                 messageElement.style.display = 'none';
             }
-        }, 1500); 
+        }, 5000);
 
         // 이벤트 발생 시간 설정
         let eventOccurred = false;
 
         // 인식 버튼 클릭 이벤트
         const recognitionButton = document.querySelector('.btn-camera');
-        recognitionButton.addEventListener('click', async function () {
+        recognitionButton.addEventListener('click', async function() {
             if (!eventOccurred) {
                 eventOccurred = true;
                 await init();
@@ -42,13 +43,13 @@
         // 웹캠 초기화 함수
         async function init() {
             // 안내문구 자동 삭제
-            setTimeout(function () {
+            setTimeout(function() {
                 var messageElement = document.querySelector('.message');
                 // 웹사이트에 처음 들어왔을 때만 (그 이후로 X)
                 if (messageElement) {
                     messageElement.style.display = 'none';
                 }
-            }, 2000); // 2초
+            }, 5000); // 5초
 
             // Teachable Machine 모델 URL
             const URL = "https://teachablemachine.withgoogle.com/models/wpm7vVOQJ/";
@@ -70,26 +71,26 @@
             window.requestAnimationFrame(loop);
             document.getElementById("webcam-container").appendChild(webcam.canvas);
 
-            // 8초 대기 후 예측 시작
-            setTimeout(async function () {
+            // 3초 대기 후 예측 시작
+            setTimeout(async function() {
                 if (!predictionMade) {
                     const result = await predict();
                     // 만약 인식률이 30% 이하라면 인식 안내 페이지로 이동
                     if (result.highestProbability <= 0.3) {
-                        setTimeout(function () {
+                        setTimeout(function() {
                             window.location.href = 'recofailure.php'; // 10초 후에 페이지 이동
-                        }, 10000); // 10초
+                        }, 3000); // 3초
                     } else {
                         sendResultToServer(result);
-                        setTimeout(function () {
+                        setTimeout(function() {
                             window.location.href = 'recosuccess.php'; // 10초 후에 페이지 이동
-                        }, 10000); // 10초
+                        }, 3000); // 3초
                     }
                     predictionMade = true;
                     // 결과 출력
                     console.log("가장 높은 인식 결과:", result);
                 }
-            }, 5000); // 5초
+            }, 3000); // 3초
 
             // 웹캠 프레임 업데이트 및 예측 함수
             async function loop() {
@@ -110,7 +111,10 @@
                     }
                 }
                 // 가장 높은 확률을 객체에 담아 반환
-                return { highestProbability, highestClass };
+                return {
+                    highestProbability,
+                    highestClass
+                };
             }
 
             // 서버로 결과를 전송하는 함수
@@ -134,7 +138,10 @@
 
 <body>
     <!-- 안내문구 -->
-    <div class="message">버릴 쓰레기를 인식해주세요!</div>
+    <div class="message">
+        5초 뒤 카메라가 켜집니다.<br>
+        3초동안 인식되니 쓰레기를 카메라 앞으로 가져가세요.
+    </div>
 
     <!-- 인식버튼 -->
     <button class="btn-camera" onclick="init()"> </button>
@@ -142,4 +149,5 @@
     <!-- 웹캠과 인식 결과를 표시할 컨테이너 -->
     <div id="webcam-container"></div>
 </body>
+
 </html>
